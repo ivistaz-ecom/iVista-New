321640fcee749dd0e055893484795c6ab7db2ca6
Sibiraj Kandhasamy <sibirajkandhasamy@IVMB-M2-Sibi-Raj-23.local>
2025-04-07 21:43:41 +0530

    new landing page created

---
+++ components/ITandTechFirmsComponent/Banner.js
@@ -0,0 +1,37 @@
+// components/Banner.js
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Banner = () => {
+  return (
+    <div className="position-relative w-100">
+      {/* Desktop video */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-100 d-none d-md-block"
+        style={{ height: '100vh', objectFit: 'cover' }}
+      >
+        <source src="/videos/tech-landing-banner.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      {/* Mobile video */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-100 d-block d-md-none"
+        style={{ height: '100vh', objectFit: 'cover' }}
+      >
+        <source src="/videos/tech-landing-banner-mobile.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ components/ITandTechFirmsComponent/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Banner from './Banner'
+import ItAndTechFirms from './ItAndTechFirms'
+import DigitalMarketing from './DigitalMarketing'
+import OurServices from './OurServices'
+import GetInTouchForm from './GetInTouchForm'
+const index = () => {
+  return (
+    <div>
+      <Banner />
+      <ItAndTechFirms />
+      <DigitalMarketing />
+      <OurServices />
+      <GetInTouchForm />
+    </div>
+  )
+}
+
+export default index
