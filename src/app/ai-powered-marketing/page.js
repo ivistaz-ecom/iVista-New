8134369729d1ec66a5805c4cc8a9076132246207
Sibiraj Kandhasamy <sibirajkandhasamy@Mac.localdomain>
2025-04-10 16:18:51 +0530

    meta title and description updated in New landing page (AI powered marketing)

--- src/app/ai-powered-marketing/page.js
+++ src/app/ai-powered-marketing/page.js
@@ -1,15 +1,35 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
 import ItAndTechFirms from '../../../components/ITandTechFirmsComponent/index'
 import Header1 from '../../../components/HeaderBlack'
 import Footer from '../../../components/Footer'
-const page = () => {
+import SeoComponents from "../../../components/SeoComponents/Seo";
+import { usePathname } from "next/navigation";
+
+function page() {
+    const pathname = usePathname();
+    const [domainName, setDomainName] = useState("");
+
+    const title = "AI-Powered Marketing for Mid-Sized IT & Tech Firms | iVistaz​";
+    const description =
+        "Transform your digital marketing with AI. iVistaz helps mid-sized IT and tech firms shift from traditional outreach to real-time, decision-shaping strategies.";
+    const path = `${pathname}`;
+    const metaImage = "";
+
     return (
-        <div>
+        <>
+            <SeoComponents
+                title={title}
+                description={description}
+                path={path}
+                metaImage={metaImage}
+            />
             <Header1 />
             <ItAndTechFirms />
             <Footer />
-        </div>
-    )
+        </>
+    );
 }
 
-export default page
+export default page;
